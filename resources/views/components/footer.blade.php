4ad0661ef2d1076e589b874ea3c669e6e10c6266
Artur1101 <stoun303@gmail.com>
2023-09-07 14:33:24 +0300

    Artur

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-    <title>404</title>
+    <title>footer</title>
     <meta name="format-detection" content="telephone=no">
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
